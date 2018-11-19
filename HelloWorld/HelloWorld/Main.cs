142c9b7f7012b194589f98c01f2a23c83f58057c
TopA-Teacher <TopA-Teacher@DESKTOP-HJNBQEA>
2018-11-19 14:58:26 +0900

    wpf day1 example

---
+++ HelloWorld/HelloWorld/Main.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+namespace HelloWorld
+{
+    class MyMain
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Window mainWindow = new Window();
+            mainWindow.Title = "WPF Sample(Main)";
+            mainWindow.MouseDown += WinMouseDown;
+            for (int i = 0; i < 2; i++)
+            {
+                Window win = new Window();
+                win.Title = "Extra Window No." + (i + 1);
+                win.ShowInTaskbar = false;
+                win.Owner = mainWindow;
+                win.Show();
+            }
+            // WPF Samplw(Main) 윈도우를 맨 마지막에 띄움
+            Application app = new Application();
+            app.Run(mainWindow);
+        }
+  
+        static void WinMouseDown(Object sender, 
+                               MouseEventArgs args)
+        {
+            Window win = new Window();
+            win.Title = "Modal DialogBox";
+            win.Width = 400;
+            win.Height = 200;
+
+            Button b = new Button();
+            b.Content = "Click Me!";
+            b.Click += Button_Click;
+            win.Content = b;
+            win.ShowDialog(); //Modal 형태
+        }
+        static private void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button Click!", sender.ToString());
+        }
+    }
+}
